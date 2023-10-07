9b7f424a19e9835e8e064b2ed0bc03e6c29fb00e
Cerberus <hugh.bill@icloud.com>
2023-10-07 15:07:12 +1300

    Test of basic 2D game

--- src/test.cpp
+++ src/test.cpp
@@ -14,13 +14,37 @@ int main( int argc, char* args[] )
 	if (win == NULL) { std::cout << "Error window creation"; }
 	SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 	if (renderer == NULL) { std::cout << "Error renderer creation"; }
+	SDL_Rect rect = { 100,100,100,100 };
 	while (run) {
+		
 		SDL_Event e;
 		if (SDL_PollEvent(&e)) {
+			SDL_RenderClear(renderer);
 			if (e.type == SDL_QUIT) {
 				run = false;
 			}
+			if (e.type == SDL_KEYDOWN) {
+				if (e.key.keysym.sym == SDLK_w) {
+					rect.y -= 10;
+				}
+				if (e.key.keysym.sym == SDLK_a) {
+					rect.x -= 10;
+				}
+				if (e.key.keysym.sym == SDLK_s) {
+					rect.y += 10;
+				}
+				if (e.key.keysym.sym == SDLK_d) {
+					rect.x += 10;
+				}
+			}
 		}
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderFillRect(renderer, &rect);
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+		SDL_RenderPresent(renderer);
+		SDL_Delay(10);	
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(win);
